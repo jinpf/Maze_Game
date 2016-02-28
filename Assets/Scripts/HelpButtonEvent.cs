1b66411373c537b1d4c112a1e9bbc9a246dfcc5c
第肆人称 <jpflcj@sina.com>
2016-02-28 10:24:09 +0800

    添加了简单的帮助说明
    添加了简单的帮助说明

---
+++ Assets/Scripts/HelpButtonEvent.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpButtonEvent : MonoBehaviour {
+
+    public GameObject helpPanel;
+    public GameObject[] helps;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            handleHelpButton();
+        }
+    }
+
+	public void handleHelpButton()
+    {
+        if (helpPanel.activeInHierarchy)
+        {
+            helpPanel.SetActive(false);
+            for (int i = 0; i < helps.Length; ++i)
+            {
+                helps[i].SetActive(false);
+            }
+
+            Debug.Log("disable!");
+        }
+        else
+        {
+            helpPanel.SetActive(true);
+            for (int i = 0; i < helps.Length; ++i)
+            {
+                helps[i].SetActive(true);
+            }
+            Debug.Log("enable!");
+        }
+    }
+}
